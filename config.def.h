75be2c3868857bd6c1d2249e0542789469c355c1
Morten Klim Sørensen <mail@kl1m.dk>
2026-03-11 14:17:51 +0100

    feat(patch): nextlayout

--- config.def.h
+++ config.def.h
@@ -49,6 +49,7 @@ static const Layout layouts[] = {
 	{ "[]=",      tile },
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ NULL,       NULL }, /* terminate */
 };
 
 /* monitors */
@@ -154,6 +155,7 @@ static const Key keys[] = {
 	{ MODKEY,                    XKB_KEY_f,           setlayout,        {.v = &layouts[1]} },
 	{ MODKEY,                    XKB_KEY_m,           setlayout,        {.v = &layouts[2]} },
 	{ MODKEY,                    XKB_KEY_space,       setlayout,        {0} },
+	{ MODKEY,                    XKB_KEY_n,           nextlayout,       {0} },
 	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_space,       togglefloating,   {0} },
 	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_t,           settiling,        {0} },
 	{ MODKEY,                    XKB_KEY_e,           togglefullscreen, {0} },
